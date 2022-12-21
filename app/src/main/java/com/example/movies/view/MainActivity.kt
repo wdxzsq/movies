795b2a14065ab6d2ba4fab29f898e418cb1f4b6c
Александров Михаил <wdxzsqq@gmail.com>
2022-12-21 17:46:57 +0300

    Start project

---
+++ app/src/main/java/com/example/movies/view/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.movies.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.example.movies.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        }
+
+    }
